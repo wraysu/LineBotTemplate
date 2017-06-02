bdcf3d402a9194a956ac5d2923713ff8c59e0dc2
wraysu <wraysu@gmail.com>
2017-06-02 20:43:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			log.Println("0001"+event.Message)
+			log.Println(event.Message)
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("NCDR"+message.Text+" OK!")).Do(); err != nil {
